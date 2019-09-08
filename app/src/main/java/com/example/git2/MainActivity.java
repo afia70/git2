1a46c45b3e7c01d46d9befe0bcadd5eb90e0c312
afia.anjum70 <afia.anjum70@gmail.com>
2019-09-08 22:48:02 +0600

    comment by B

--- app/src/main/java/com/example/git2/MainActivity.java
+++ app/src/main/java/com/example/git2/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //This is 1st comment by A
+    // Do not need this
     //This is 1st comment by B
 }
